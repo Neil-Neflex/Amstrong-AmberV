a172135d11bcaf748e11173e16396761f0148cb5
Neil-Neflex <amrane.neil@gmail.com>
2023-03-06 13:39:57 +0100

    vvv
    gggff

---
+++ CODE/Main.ino
@@ -0,0 +1,87 @@
+#include <Servo.h>
+
+Servo thumb;
+Servo index;
+Servo major;
+Servo ring;
+Servo annular;
+
+int i;
+int j;
+Servo wrist;
+
+Servo hand[]={thumb, index, major, ring, annular};
+
+void setup() {
+
+    for(i=2; i<7;i++){
+        hand[i].attach(i);
+    }
+    wrist.attach(7);
+    wrist.write(20);
+}
+
+void loop() {
+ 
+    for (i=0;i<5;i++){
+      hand[i].write(170);
+      delay(100);
+    }
+    delay(2000);
+    
+    for (i=0;i<5;i++){
+        hand[i].write(70);
+        delay(100);
+    }  
+    delay(2000);
+    fuxx();
+
+    delay(2000);
+    like();
+
+    delay(2000);
+    coucou();
+
+    delay(2000);
+  }
+
+ void fuxx(){
+    for(i=0;i<5;i++){
+        hand[i].write(175);
+        delay(100);
+    }
+    major.write(60);
+ }
+
+void like(){
+    for (i=0;i<5;i++){
+        hand[i].write(60);
+        delay(100);
+    }
+    major.write(180);
+    ring.write(180);  
+}
+
+void okay(){
+    //revoir angle pouce index + retendre les cables
+    for (i=0;i<5;i++){
+        hand[i].write(60);
+        delay(100);
+    }
+    thumb.write(170);
+    index.write(140);
+}
+
+void coucou(){
+    for (i=0;i<5;i++){
+        hand[i].write(60);
+        delay(100);
+    }
+    for (i=0;i<4;i++){
+        wrist.write(0);
+        delay(500);
+        wrist.write(40);
+        delay(500);
+    }
+    wrist.write(20);
+}
